be2617f6b2952f2ebebaa6ccab532819c744794a
Estevão Ferreira <tevonrs@gmail.com>
2023-06-29 19:20:36 -0300

    as

--- loja/settings.py
+++ loja/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jd)252ki6cqtxw0^ctn9+d!w92zzfcysdlsmyuk^o9j8dt3c(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['54.89.141.223']
 

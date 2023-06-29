b56abc710e3f60e2ed2be4c9f55014d9e435d300
Estevão Ferreira <tevonrs@gmail.com>
2023-06-29 19:43:38 -0300

    teste

--- loja/settings.py
+++ loja/settings.py
@@ -150,9 +150,9 @@ DATE_FORMAT = 'd/m/Y'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'templates')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join('templates/static')
+    os.path.join(BASE_DIR, 'templates'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

c47163bea37a5bbb6d8ac869d068806421aa925f
Estevão Ferreira <tevonrs@gmail.com>
2023-06-29 19:03:06 -0300

    alowed hosts e debug

--- loja/settings.py
+++ loja/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jd)252ki6cqtxw0^ctn9+d!w92zzfcysdlsmyuk^o9j8dt3c(^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.89.141.223']
 
 
 # Application definition

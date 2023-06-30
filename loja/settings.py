883ea67e31ed7930389a5d1613771898d63f286f
Estevão Ferreira <tevonrs@gmail.com>
2023-06-30 00:42:22 -0300

    database

--- loja/settings.py
+++ loja/settings.py
@@ -102,8 +102,12 @@ WSGI_APPLICATION = 'loja.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'american_data_base',
+        'USER': 'american_brand_postgres',
+        'PASSWORD': 'teste1234@rt',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
